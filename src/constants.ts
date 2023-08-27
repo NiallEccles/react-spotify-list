5b2b31eec5975b82d98e3ad6978c35009f07a7a2
EcclesN <Niall.Eccles@hmhco.com>
2023-08-27 16:01:41 +0100

    Update redirect

--- src/constants.ts
+++ src/constants.ts
@@ -4,6 +4,6 @@ interface SPOTIFY_CONSTANTS {
 }
 
 export const SPOTIFY: SPOTIFY_CONSTANTS = {
-    REDIRECT_URI: 'http://localhost:5173/auth/',
+    REDIRECT_URI: 'https://react-spotify-list-9se9pwuvh-nialleccles.vercel.app/auth/',
     CLIENT_ID: '591d4e02e88a4e20a1daf9d59f2a46db'
 }
