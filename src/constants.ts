91554e9a5c34da1a6875eb6cb1fbcfdab5440183
EcclesN <Niall.Eccles@hmhco.com>
2023-08-28 23:10:36 +0100

    Create const for local and live redirect uri

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,11 @@
 interface SPOTIFY_CONSTANTS {
+    LOCAL_REDIRECT_URI: string,
     REDIRECT_URI: string,
     CLIENT_ID: string,
 }
 
 export const SPOTIFY: SPOTIFY_CONSTANTS = {
-    REDIRECT_URI: 'http://localhost:5173/auth/',
-    // REDIRECT_URI: 'https://react-spotify-list.vercel.app/auth/',
+    LOCAL_REDIRECT_URI: 'http://localhost:5173/auth/',
+    REDIRECT_URI: 'https://react-spotify-list.vercel.app/auth/',
     CLIENT_ID: '591d4e02e88a4e20a1daf9d59f2a46db'
 }
