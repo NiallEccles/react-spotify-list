7ccc9ede0a4f10056d628432f938dcddd3b3e803
EcclesN <Niall.Eccles@hmhco.com>
2023-08-30 23:34:22 +0100

    Attempt to create collate queries

--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -1,14 +1,20 @@
-import { Header } from "../components/Header"
-import { TopArtists } from "../components/TopArtists"
-import { TopSongs } from "../components/TopSongs"
-
+import { useQueries } from "react-query"
+// import { TopArtists } from "../components/TopArtists"
+// import { TopSongs } from "../components/TopSongs"
+import { getMe, getTopArtists, getTopTracks } from "../queries"
 
 export const Top = () => {
+    const results = useQueries([
+        { queryKey: ['me', 1], queryFn: getMe, staleTime: Infinity},
+        { queryKey: ['top artists', 2], queryFn: getTopArtists, staleTime: Infinity},
+        { queryKey: ['top tracks', 3], queryFn: getTopTracks, staleTime: Infinity}
+    ]);
+    console.log(results);
     return (
         <>
-            <Header/>
-            <TopArtists/>
-            <TopSongs/>
+            {/* <Header/> */}
+            {/* <TopArtists/> */}
+            {/* <TopSongs/> */}
         </>
     )
 }
