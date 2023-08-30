e3e96d6766596c979c3833111b5df4cc222fdaae
EcclesN <Niall.Eccles@hmhco.com>
2023-08-30 23:33:55 +0100

    Add Spotify endpoints

--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,23 @@
-interface SPOTIFY_CONSTANTS {
-    LOCAL_REDIRECT_URI: string,
-    REDIRECT_URI: string,
-    CLIENT_ID: string,
+type SPOTIFY_CONSTANTS = {
+    LOCAL_REDIRECT_URI: string;
+    REDIRECT_URI: string;
+    CLIENT_ID: string;
+    API: {
+        ROOT: string;
+        ME: string;
+        TOP_ARTISTS: string;
+        TOP_TRACKS: string;
+    }
 }
 
 export const SPOTIFY: SPOTIFY_CONSTANTS = {
     LOCAL_REDIRECT_URI: 'http://localhost:5173/auth/',
     REDIRECT_URI: 'https://react-spotify-list.vercel.app/auth/',
-    CLIENT_ID: '591d4e02e88a4e20a1daf9d59f2a46db'
+    CLIENT_ID: '591d4e02e88a4e20a1daf9d59f2a46db',
+    API: {
+        ROOT: 'https://api.spotify.com/v1/',
+        ME: 'https://api.spotify.com/v1/me/',
+        TOP_ARTISTS: 'https://api.spotify.com/v1/me/top/artists',
+        TOP_TRACKS: 'https://api.spotify.com/v1/me/top/tracks'
+    },
 }
