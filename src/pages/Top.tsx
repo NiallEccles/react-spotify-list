6fa79ed344dcb922ddcc710d4b5fcdb3f5d5cf73
EcclesN <Niall.Eccles@hmhco.com>
2023-08-31 01:19:05 +0100

    Collate queries and pass return data into components

--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -1,20 +1,38 @@
 import { useQueries } from "react-query"
-// import { TopArtists } from "../components/TopArtists"
-// import { TopSongs } from "../components/TopSongs"
-import { getMe, getTopArtists, getTopTracks } from "../queries"
+import { Header } from "../components/Header"
+import { TopArtists } from "../components/TopArtists"
+import { TopSongs } from "../components/TopSongs"
+import { query } from "../queries"
+import { useStore } from "../state"
+import { SPOTIFY } from "../constants";
 
 export const Top = () => {
-    const results = useQueries([
+    const { access_token } = useStore();
+
+    const getHeaders = (accessToken: string) => {
+        return {
+            headers: {
+                Authorization: 'Bearer ' + accessToken,
+            }
+        }
+    }
+
+    const getMe = async () => query(SPOTIFY.API.ME, getHeaders(access_token ? access_token : ''));
+    const getTopArtists = async () => query(SPOTIFY.API.TOP_ARTISTS, getHeaders(access_token ? access_token : ''));
+    const getTopTracks = async () => query(SPOTIFY.API.TOP_TRACKS, getHeaders(access_token ? access_token : ''));
+
+    const [me, topArtists, topTracks] = useQueries([
         { queryKey: ['me', 1], queryFn: getMe, staleTime: Infinity},
         { queryKey: ['top artists', 2], queryFn: getTopArtists, staleTime: Infinity},
         { queryKey: ['top tracks', 3], queryFn: getTopTracks, staleTime: Infinity}
     ]);
-    console.log(results);
+
+    console.log(me, topArtists, topTracks);
     return (
         <>
-            {/* <Header/> */}
-            {/* <TopArtists/> */}
-            {/* <TopSongs/> */}
+            <Header data={me.data}/>
+            <TopArtists items={topArtists.data.items}/>
+            <TopSongs items={topTracks.data.items}/>
         </>
     )
 }
