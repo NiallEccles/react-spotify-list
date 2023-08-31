2d35fbf260808755a176f6c01d296b1d8171ca4f
EcclesN <Niall.Eccles@hmhco.com>
2023-08-31 01:16:47 +0100

    Create query helper

--- src/queries/index.ts
+++ src/queries/index.ts
@@ -1,20 +1,10 @@
-import { SPOTIFY } from "../constants";
-
-const accessToken = localStorage.getItem('access_token');
-
-const HEADERS = {
-    headers: {
-        Authorization: 'Bearer ' + accessToken,
-    }
-}
-
-export const query = async (endpoint: string) => {
-    const response = await fetch(endpoint, HEADERS);
+export const query = async (endpoint: string, headers: object) => {
+    const response = await fetch(endpoint, headers);
     const returnData = await response.json();
 
     return returnData;
 };
 
-export const getMe = async () => query(SPOTIFY.API.ME);
-export const getTopArtists = async () => query(SPOTIFY.API.TOP_ARTISTS);
-export const getTopTracks = async () => query(SPOTIFY.API.TOP_TRACKS);
+// export const getMe = async () => query(SPOTIFY.API.ME);
+// export const getTopArtists = async () => query(SPOTIFY.API.TOP_ARTISTS);
+// export const getTopTracks = async () => query(SPOTIFY.API.TOP_TRACKS);
