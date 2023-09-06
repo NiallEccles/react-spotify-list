ecbdf4f157aa14493852fbcc7eeb0077575d3402
EcclesN <Niall.Eccles@hmhco.com>
2023-09-06 21:51:04 +0100

    Styling updates

--- src/components/TopArtists.tsx
+++ src/components/TopArtists.tsx
@@ -8,7 +8,12 @@ export const TopArtists: React.FC<{items: Item[]}> = ({items}) => {
             <ol>
                 {
                     items && items.map((artist: Item, index: number) => {
-                        return index < 10 ? <li key={artist.name}>{artist.name}</li> : null
+                        return index < 10 ? 
+                            <li className="flex flex-row items-center mb-4 justify-between" key={artist.name}>
+                                <h4 className="text-5xl font-bold">#{++index}</h4>
+                                <img className="w-32" src={artist.images[2].url} alt="" />
+                                <h4 className="w-32 text-xl font-bold">{artist.name}</h4>
+                            </li> : null
                     })
                 }
             </ol>
