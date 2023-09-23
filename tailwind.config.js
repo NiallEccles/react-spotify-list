477c47279cf4885720e5f9932aa8ac5a95b26242
EcclesN <Niall.Eccles@hmhco.com>
2023-09-23 17:55:58 +0100

    Update Tailwinds gridTemplateColumns 2 to be custom

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        '2': '3.5rem 1fr'
+      }
+    },
   },
   plugins: [],
 }
