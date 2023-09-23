23a8ca7f1ae29ffdf15a9830ce31aedea5b59d87
EcclesN <Niall.Eccles@hmhco.com>
2023-09-23 11:11:13 +0100

    Updated style of button

--- src/components/AuthLink.tsx
+++ src/components/AuthLink.tsx
@@ -3,13 +3,18 @@ import { useSpotifyAuthUrl } from "../hooks/useSpotifyAuthUrl";
 import { FormattedMessage } from "react-intl";
 
 export const AuthLink = () => {
-    const [authUrl, setAuthUrl] = useState<string|null>(null);
-    const { url } = useSpotifyAuthUrl();
+  const [authUrl, setAuthUrl] = useState<string | null>(null);
+  const { url } = useSpotifyAuthUrl();
 
-    useEffect(() => {
-      setAuthUrl(url);
-    }, [url]);
-    return (
-        authUrl && <div className="h-full flex flex-row items-center"><a className="block text-white text-2xl p-2 h-12 text-center rounded-full w-full bg-black" href={authUrl}><h3><FormattedMessage id="loginWithSpotify" /></h3></a></div>
-    )
+  useEffect(() => {
+    setAuthUrl(url);
+  }, [url]);
+  return (
+    authUrl && <div className="h-full flex flex-row items-center">
+      <a className="block text-black text-xl p-2 h-12 text-center w-full bg-rose-200 border-black border-2 hover:bg-rose-300 transition-colors ease-in-out" href={authUrl}>
+        <h3>
+          <FormattedMessage id="loginWithSpotify" />
+        </h3>
+      </a></div>
+  )
 }
