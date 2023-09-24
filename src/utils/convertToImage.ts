cd85ffc44b1ed417c7491399d8e5bff267021998
EcclesN <Niall.Eccles@hmhco.com>
2023-09-24 22:23:35 +0100

    Testing image download

--- src/utils/convertToImage.ts
+++ src/utils/convertToImage.ts
@@ -1,4 +1,5 @@
-import { toPng } from 'html-to-image';
+import { toBlob } from 'html-to-image';
+import { saveAs } from 'file-saver';
 
 export const convertToImage = async (HTMLElement: React.MutableRefObject<HTMLElement | null>, name: string) => {
     const element = HTMLElement.current;
@@ -6,19 +7,22 @@ export const convertToImage = async (HTMLElement: React.MutableRefObject<HTMLEle
     
 
     if (element) {
-        await toPng(element, { cacheBust: true })
-            .then((dataUrl) => {
-                new Promise((resolve)=>{
-                    setTimeout(resolve, 1100);
-                })
-                const link = document.createElement("a");
-                link.download = `${new Date().toLocaleTimeString()}.png`;
-                link.href = dataUrl;
-                link.click();
-            })
-            .catch((err) => {
-                console.log(err);
-            });
+        await toBlob(element).then((blob) => {
+            saveAs(blob!, 'MY_BLOB.png');
+        });
+        // await toPng(element, { cacheBust: true })
+        //     .then((dataUrl) => {
+        //         new Promise((resolve)=>{
+        //             setTimeout(resolve, 1100);
+        //         })
+        //         const link = document.createElement("a");
+        //         link.download = `${new Date().toLocaleTimeString()}.png`;
+        //         link.href = dataUrl;
+        //         link.click();
+        //     })
+        //     .catch((err) => {
+        //         console.log(err);
+        //     });
     } else {
         console.error("Ref is null");
     }
