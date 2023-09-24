52893c9ebbc533fe4cc714899cb6c17ede675833
EcclesN <Niall.Eccles@hmhco.com>
2023-09-24 15:11:34 +0100

    Testing image download

--- src/components/TopArtists.tsx
+++ src/components/TopArtists.tsx
@@ -1,32 +1,32 @@
 import { FormattedMessage } from "react-intl";
 import { Item } from "../types/TopArtists";
-import { useRef } from "react";
-import { convertToImage } from "../utils/convertToImage";
+// import { useRef } from "react";
+// import { convertToImage } from "../utils/convertToImage";
 import { Download } from "lucide-react";
-// import { useToPng } from '@hugocxl/react-to-image';
+import { useToJpeg } from '@hugocxl/react-to-image';
 
 export const TopArtists: React.FC<{ items: Item[] }> = ({ items }) => {
-    const elementRef = useRef<HTMLElement | null>(null);
+    // const elementRef = useRef<HTMLElement | null>(null);
 
-    const handleClick = () => convertToImage(elementRef, 'Top Artists');
+    // const handleClick = () => convertToImage(elementRef, 'Top Artists');
     // const handleClick = () => toCanvas(elementRef);
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    // const [_, convert, ref] = useToPng<HTMLDivElement>({
-    //     quality: 1,
-    //     onSuccess: data => {
-    //       const link = document.createElement('a');
-    //       link.download = 'TopArtistsNew.jpeg';
-    //       link.href = data;
-    //       link.click();
-    //     }
-    //   });
+    const [_, convert, ref] = useToJpeg<HTMLDivElement>({
+        quality: 1,
+        onSuccess: data => {
+          const link = document.createElement('a');
+          link.download = 'TopArtistsNew.';
+          link.href = data;
+          link.click();
+        }
+      });
 
     return (
-        <section className="mb-10" ref={elementRef}>
+        <section className="mb-10" ref={ref}>
             <h3 className="text-3xl p-2 italic font-bold flex justify-between bg-rose-200 border-black border-2 border-b-0 hover:bg-rose-300 transition-colors ease-in-out">
                 <FormattedMessage id="topArtists" />
-                <span className="w-8 h-8" onClick={handleClick}>
+                <span className="w-8 h-8" onClick={convert}>
                     <Download width={'100%'} height={'100%'} />
                 </span>
             </h3>
