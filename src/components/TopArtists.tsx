f54da34c9b3f0780f0f491eeba33f46d6ef0e1fa
EcclesN <Niall.Eccles@hmhco.com>
2023-09-24 15:28:10 +0100

    Testing image download

--- src/components/TopArtists.tsx
+++ src/components/TopArtists.tsx
@@ -1,14 +1,14 @@
 import { FormattedMessage } from "react-intl";
 import { Item } from "../types/TopArtists";
-// import { useRef } from "react";
-// import { convertToImage } from "../utils/convertToImage";
+import { useRef } from "react";
+import { convertToImage } from "../utils/convertToImage";
 import { Download } from "lucide-react";
 import { useToPng } from '@hugocxl/react-to-image';
 
 export const TopArtists: React.FC<{ items: Item[] }> = ({ items }) => {
-    // const elementRef = useRef<HTMLElement | null>(null);
+    const elementRef = useRef<HTMLElement | null>(null);
 
-    // const handleClick = () => convertToImage(elementRef, 'Top Artists');
+    const handleClick = () => convertToImage(elementRef, 'Top Artists');
     // const handleClick = () => toCanvas(elementRef);
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -24,10 +24,10 @@ export const TopArtists: React.FC<{ items: Item[] }> = ({ items }) => {
       });
 
     return (
-        <section className="mb-10" ref={ref}>
+        <section className="mb-10" ref={elementRef}>
             <h3 className="text-3xl p-2 italic font-bold flex justify-between bg-rose-200 border-black border-2 border-b-0 hover:bg-rose-300 transition-colors ease-in-out">
                 <FormattedMessage id="topArtists" />
-                <span className="w-8 h-8" onClick={convert}>
+                <span className="w-8 h-8" onClick={handleClick}>
                     <Download width={'100%'} height={'100%'} />
                 </span>
             </h3>
