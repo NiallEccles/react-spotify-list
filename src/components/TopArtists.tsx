e2c647804189310a1b1cea4dd4a0fc4c5152e140
EcclesN <Niall.Eccles@hmhco.com>
2023-09-24 20:29:23 +0100

    Testing image download

--- src/components/TopArtists.tsx
+++ src/components/TopArtists.tsx
@@ -3,11 +3,22 @@ import { Item } from "../types/TopArtists";
 import { useRef } from "react";
 import { convertToImage } from "../utils/convertToImage";
 import { Download } from "lucide-react";
+// import { captureElementAsImage } from "../utils/captureElementAsImage";
 // import { useToPng } from '@hugocxl/react-to-image';
 
 export const TopArtists: React.FC<{ items: Item[] }> = ({ items }) => {
     const elementRef = useRef<HTMLElement | null>(null);
 
+    // const handleClick = () => captureElementAsImage(elementRef.current);
+    // const handleClick = async () => {
+    //     const imageDataURL = await captureElementAsImage(elementRef.current);
+    //     if (imageDataURL) {
+    //       const link = document.createElement('a');
+    //       link.href = imageDataURL;
+    //       link.download = 'captured_image.png';
+    //       link.click();
+    //     }
+    //   };
     const handleClick = () => convertToImage(elementRef, 'Top Artists');
     // const handleClick = () => toCanvas(elementRef);
 
@@ -39,7 +50,8 @@ export const TopArtists: React.FC<{ items: Item[] }> = ({ items }) => {
                                 <div className="relative">
                                     <h4 className="text-xs w-6 h-6 font-bold absolute z-10 bg-white rounded-full -right-3 -bottom-2 flex items-center justify-center">#{++index}</h4>
                                     {/* <img className="w-14 h-14 object-fill border-black border-2" src={artist.images[2].url} alt="" /> */}
-                                    <div className="w-14 h-14 object-fill border-black border-2 bg-cover" style={{backgroundImage: `url(${artist.images[2].url})`}}></div>
+                                    {/* <div className="w-14 h-14 object-fill border-black border-2 bg-cover" style={{backgroundImage: `url(${artist.images[2].url})`}}></div> */}
+                                    <img className="w-14 h-14 object-fill border-black border-2 bg-cover" src={artist.images[2].url} alt="" crossOrigin="anonymous"/>
                                 </div>
                                 <div className="flex flex-col justify-center w-full ml-4 truncate">
                                     <h4 className="w-60 text-xl font-bold truncate">{artist.name}</h4>
