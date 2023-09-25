46e512337e8942764738a67c1d4bcc746af94fee
EcclesN <Niall.Eccles@hmhco.com>
2023-09-25 21:17:02 +0100

    Fixed Safari image rendering issue

--- src/utils/convertToImage.ts
+++ src/utils/convertToImage.ts
@@ -1,5 +1,5 @@
-import { toBlob } from 'html-to-image';
-import { saveAs } from 'file-saver';
+import { toPng } from 'html-to-image';
+// import { saveAs } from 'file-saver';
 
 export const convertToImage = async (HTMLElement: React.MutableRefObject<HTMLElement | null>, name: string) => {
     const element = HTMLElement.current;
@@ -7,22 +7,26 @@ export const convertToImage = async (HTMLElement: React.MutableRefObject<HTMLEle
     
 
     if (element) {
-        await toBlob(element).then((blob) => {
-            saveAs(blob!, 'MY_BLOB.png');
+        // await toBlob(element).then((blob) => {
+        //     saveAs(blob!, 'MY_BLOB.png');
+        // });
+        // Because Safari is bad
+        // I need to call this
+        // multiple times
+        // so the canvas images
+        // are rendered
+        await toPng(element, { cacheBust: true });
+        await toPng(element, { cacheBust: true });
+        await toPng(element, { cacheBust: true })
+        .then((dataUrl) => {
+            const link = document.createElement("a");
+            link.download = `${new Date().toLocaleTimeString()}.png`;
+            link.href = dataUrl;
+            link.click();
+        })
+        .catch((err) => {
+            console.log(err);
         });
-        // await toPng(element, { cacheBust: true })
-        //     .then((dataUrl) => {
-        //         new Promise((resolve)=>{
-        //             setTimeout(resolve, 1100);
-        //         })
-        //         const link = document.createElement("a");
-        //         link.download = `${new Date().toLocaleTimeString()}.png`;
-        //         link.href = dataUrl;
-        //         link.click();
-        //     })
-        //     .catch((err) => {
-        //         console.log(err);
-        //     });
     } else {
         console.error("Ref is null");
     }
