d45bfd684cc164d97dc1119a433020c5af9c8c20
EcclesN <Niall.Eccles@hmhco.com>
2023-09-26 23:18:33 +0100

    Use new utils for sharing and downloading

--- src/components/TopArtists.tsx
+++ src/components/TopArtists.tsx
@@ -1,27 +1,43 @@
 import { FormattedMessage } from "react-intl";
 import { Item } from "../types/TopArtists";
-import { useRef, useState } from "react";
+import { useRef } from "react";
 import { convertToImage } from "../utils/convertToImage";
-import { Download } from "lucide-react";
-import { Share } from "./Share";
+import { Download, Share2 } from "lucide-react";
+import { downloadImage } from "../utils/downloadImage";
+import { shareImage } from "../utils/shareImage";
 
 export const TopArtists: React.FC<{ items: Item[] }> = ({ items }) => {
     const elementRef = useRef<HTMLElement | null>(null);
 
     // eslint-disable-next-line  @typescript-eslint/no-explicit-any
-    const [image, setImage] = useState<any>();
+    // const [image, setImage] = useState<any>();
 
-    const handleClick = () => setImage(convertToImage(elementRef, 'Top Artists'));
+    const handleDownload = () => 
+        convertToImage(elementRef).then(imageData => {
+            downloadImage(imageData, 'Top Artists');
+        });
+    const handleShare = () => {
+        convertToImage(elementRef).then(imageData => {
+            shareImage(imageData, 'Top Artists');
+        }).catch(error => console.log(error));
+    };
+
+    // useEffect(() => {
+    //     console.log(image);
+    // }, [image]);
 
     return (
         <section className="mb-10" ref={elementRef}>
             <h3 className="text-3xl p-2 italic font-bold flex justify-between bg-rose-200 border-black border-2 border-b-0 hover:bg-rose-300 transition-colors ease-in-out">
                 <FormattedMessage id="topArtists" />
                 <div className="flex flex-row w-20 justify-between">
-                    <span className="w-8 h-8" onClick={handleClick}>
+                    <button className="w-8 h-8" onClick={handleDownload}>
                         <Download width={'100%'} height={'100%'} />
-                    </span>
-                    <Share image={image}/>
+                    </button>
+                    <button className="w-8 h-8" onClick={handleShare}>
+                        {/* <Share image={image}/> */}
+                        <Share2 width={'100%'} height={'100%'}/>
+                    </button>
                 </div>
             </h3>
             <ol className="py-5 bg-rose-200 border-black border-2">
@@ -33,7 +49,7 @@ export const TopArtists: React.FC<{ items: Item[] }> = ({ items }) => {
                                     <h4 className="text-xs w-6 h-6 font-bold absolute z-10 bg-white rounded-full -right-3 -bottom-2 flex items-center justify-center">#{++index}</h4>
                                     {/* <img className="w-14 h-14 object-fill border-black border-2" src={artist.images[2].url} alt="" /> */}
                                     {/* <div className="w-14 h-14 object-fill border-black border-2 bg-cover" style={{backgroundImage: `url(${artist.images[2].url})`}}></div> */}
-                                    <img className="w-14 h-14 object-fill border-black border-2 bg-cover" src={artist.images[2].url} alt="" crossOrigin="anonymous"/>
+                                    <img className="w-14 h-14 object-fill border-black border-2 bg-cover" src={artist.images[2].url} alt="" crossOrigin="anonymous" />
                                 </div>
                                 <div className="flex flex-col justify-center w-full ml-4 truncate">
                                     <h4 className="w-60 text-xl font-bold truncate">{artist.name}</h4>
