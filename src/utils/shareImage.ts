73f8f0ab63c18d34b93e91539167a0978b38d591
EcclesN <Niall.Eccles@hmhco.com>
2023-09-26 23:46:37 +0100

    Remove test from share

--- src/utils/shareImage.ts
+++ src/utils/shareImage.ts
@@ -1,13 +1,14 @@
 export const shareImage = async (image: string, name: string) => {
     const blob = await (await fetch(image)).blob();
     const file = new File([blob], `${name}.png`, { type: blob.type });
+    console.log(blob.type);
+    
     if (!navigator.canShare) throw new Error("Your system doesn't support sharing files.");
     // && !navigator.canShare({ files: [file] })
 
     navigator.share({
         files: [file],
         title: name,
-        text: name,
     })
         .then(() => console.log('Share was successful.'))
         .catch((error) => console.log('Sharing failed', error));
