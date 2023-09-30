d542610174caec9ebedf7c3ee65dbcdec7f866e9
EcclesN <Niall.Eccles@hmhco.com>
2023-09-30 16:22:23 +0100

    Create loading for lists

---
+++ src/components/Loading.tsx
@@ -0,0 +1,34 @@
+export const Loading = () => {
+    const numRows = Array.from(Array(10).keys());
+    console.log(numRows);
+
+    return (
+        <>
+            <div className="mt-16 text-3xl p-2 italic font-bold flex justify-between bg-rose-200 border-black border-2 border-b-0">
+                {/* <FormattedMessage id="topArtists" /> */}
+                <div className="w-40 h-8 bg-red-400 animate-pulse"></div>
+                <div className="flex flex-row w-20 justify-between">
+                    <div className="w-8 h-8 bg-red-400 animate-pulse"></div>
+                    <div className="w-8 h-8 bg-red-400 animate-pulse"></div>
+                </div>
+            </div>
+            <div className="pt-5 bg-rose-200 border-black border-2 pr-4">
+                {
+                    numRows.map((i) => {
+                        return (
+                            <div className="grid grid-cols-2 px-4 mb-3 w-full" key={i}>
+                                <div className="relative mb-3 animate-pulse">
+                                    <h4 className="w-6 h-6 absolute z-10 bg-red-400 rounded-full -right-3 -bottom-2"></h4>
+                                    <div className="w-14 h-14 bg-red-400"></div>
+                                </div>
+                                <div className="flex flex-col justify-center w-full ml-4 mb-5 animate-pulse">
+                                    <div className="w-full h-8 bg-red-400"></div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </>
+    );
+};
