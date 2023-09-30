3cfcfd6d90cdadf50564e1872089cc56b004e9ea
EcclesN <Niall.Eccles@hmhco.com>
2023-09-30 15:00:10 +0100

    Change app name to Listur

--- src/constants.ts
+++ src/constants.ts
@@ -12,7 +12,7 @@ type SPOTIFY_CONSTANTS = {
 
 export const SPOTIFY: SPOTIFY_CONSTANTS = {
     LOCAL_REDIRECT_URI: 'http://localhost:5173/auth/',
-    REDIRECT_URI: 'https://react-spotify-list.vercel.app/auth/',
+    REDIRECT_URI: 'https://listur.vercel.app/auth/',
     CLIENT_ID: '591d4e02e88a4e20a1daf9d59f2a46db',
     API: {
         ROOT: 'https://api.spotify.com/v1/',
