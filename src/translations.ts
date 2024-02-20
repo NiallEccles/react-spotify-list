19a340af8788578de437a287f43d6be426e687c8
EcclesN <Niall.Eccles@hmhco.com>
2024-02-20 08:29:19 +0000

    Add en-US translations

--- src/translations.ts
+++ src/translations.ts
@@ -15,6 +15,16 @@ export const messages: LanguageTranslation = {
         removeAccessReturn: 'Got it, thanks!',
         removeAccessContinue: 'Spotify',
     },
+    'en-US': {
+        topArtists: 'Top Artists',
+        topTracks: 'Top Songs',
+        loginWithSpotify: 'Log in with Spotify',
+        removeAccess: 'I want to revoke access',
+        removeAccessTitle: 'Revoke Access',
+        removeAccessDescription: 'To revoke access, follow the Spotify link below. You will be presented with a list of apps. Click "Remove Access" for  "Listur"',
+        removeAccessReturn: 'Got it, thanks!',
+        removeAccessContinue: 'Spotify',
+    },
     'pt-BR': {
         topArtists: 'Principais Artistas',
         topTracks: 'Principais Faixas',
